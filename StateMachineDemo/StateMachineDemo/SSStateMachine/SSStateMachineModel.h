d0f1f781ec9ba35f7b1031d29acc67e50a8f9cba
SoulJa <soulja@jonnydeiMac.lan>
2017-06-09 11:04:17 +0800

    添加状态机Model类

---
+++ StateMachineDemo/StateMachineDemo/SSStateMachine/SSStateMachineModel.h
@@ -0,0 +1,13 @@
+//
+//  SSStateMachineModel.h
+//  StateMachineDemo
+//
+//  Created by SoulJa on 2017/6/9.
+//  Copyright © 2017年 sdp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSStateMachineModel : NSObject
+
+@end
---
+++ StateMachineDemo/StateMachineDemo/SSStateMachine/SSStateMachineModel.m
@@ -0,0 +1,13 @@
+//
+//  SSStateMachineModel.m
+//  StateMachineDemo
+//
+//  Created by SoulJa on 2017/6/9.
+//  Copyright © 2017年 sdp. All rights reserved.
+//
+
+#import "SSStateMachineModel.h"
+
+@implementation SSStateMachineModel
+
+@end
